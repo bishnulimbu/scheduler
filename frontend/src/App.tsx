a2ded2c7420cef40b80f6a7acd115b9c1700318c
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-12-19 20:35:07 +0545

    main app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Calendar } from "@/components/ui/calendar"
+// import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+// import { AppSidebar } from "@/components/app-sidebar"
+import Layout from './Layout';
 
 function App() {
-  // const [count, setCount] = useState(0)
   const [date, setDate] = useState<Date | undefined>(new Date())
   // will not need react as useState is already imported and doesn't need reference.
 
+
+
   return (
-    <Calendar
-      mode="single"
-      selected={date}
-      onSelect={setDate}
-      // className="rounded-md border"
-      className="rounded-md border text-white bg-zinc-800"
-    />
+    <>
+      {/* <SidebarProvider> */}
+      {/*   <AppSidebar /> */}
+      {/*   <main> */}
+      {/*     <SidebarTrigger /> */}
+      {/*   </main> */}
+      {/* </SidebarProvider> */}
+      <Layout>
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-md border"
+        // className="rounded-md border text-white bg-zinc-800"
+        />
+      </Layout>
+    </>
   )
 
 }
