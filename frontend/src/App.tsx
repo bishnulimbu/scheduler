6eb5e0a969eb08c146606022601e8bd31369130f
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-12-19 19:17:30 +0545

    added a calendar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,34 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Calendar } from "@/components/ui/calendar"
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [date, setDate] = useState<Date | undefined>(new Date())
+  // will not need react as useState is already imported and doesn't need reference.
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Calendar
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      // className="rounded-md border"
+      className="rounded-md border text-white bg-zinc-800"
+    />
   )
+
 }
 
 export default App
